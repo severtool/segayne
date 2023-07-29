c867335cd53779a724a6d5bacd455e49cc9e979d
severtool <129487417+severtool@users.noreply.github.com>
2023-07-29 20:16:20 +0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,4 +0,0 @@
-printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
-curl -s https://raw.githubusercontent.com/severtool/segayne/main/ddos.php -o /data/data/com.termux/files/usr/bin/vermin
-chmod 777 /data/data/com.termux/files/usr/bin/vermin
-printf '\n\033[1;92m Gõ \033[1;93mvermin \033[1;92mĐể Vào Tool <33\n\n'
