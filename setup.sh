4391bbedcfede88cf929b3fbc997dbde10e36d00
severtool <129487417+severtool@users.noreply.github.com>
2023-07-29 20:10:27 +0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
-termux-setup-storage && apt update && apt upgrade && pkg upgrade -y && pkg update -y && pkg i tur-repo -y && pkg i dichvucoder-repo -y && pkg rem tur-repo -y && pkg i php
-curl -s https://raw.githubusercontent.com/severtool/segayne/main/ddos.php -o /data/data/com.termux/files/usr/bin/vermin
-chmod 777 /data/data/com.termux/files/usr/bin/vermin
-printf '\n\033[1;92m Gõ \033[1;93mvermin \033[1;92mĐể Vào Tool <33\n\n'
