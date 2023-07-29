fb8c7878ad3624e831ae9f7b266c8472b284f50f
severtool <129487417+severtool@users.noreply.github.com>
2023-07-29 18:48:15 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,3 @@
+curl -s https://raw.githubusercontent.com/severtool/segayne/main/ddos.php -o $home/usr/bin/vermin
+chmod 777 $home/usr/bin/vermin
+printf "\033[0mSetup Thành Công, Bấm \033[1;33mvermin\033[0m Để Vào Sever\n"
