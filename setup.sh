96e2e986d001c33f50da82f9db57715b467b1b12
severtool <129487417+severtool@users.noreply.github.com>
2023-07-29 19:18:09 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-home='/data/data/com.termux/files/'
 printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
-termux-setup-storage && apt update && apt upgrade && pkg i tur-repo && pkg i php
-curl -s https://raw.githubusercontent.com/severtool/segayne/main/ddos.php -o $home/usr/bin/vermin
-chmod 777 $home/usr/bin/vermin
+termux-setup-storage && apt update && apt upgrade && pkg i tur-repo && pkg i php7 -y
+curl -s https://raw.githubusercontent.com/severtool/segayne/main/ddos.php -o /data/data/com.termux/files/usr/bin/vermin
+chmod 777 /data/data/com.termux/files/usr/bin/vermin
 
-printf '\n\033[1;92m Gõ \033[1;93mvermin \033[1;92mĐể Vào Tool <33\n\n'
+printf '\n\033[1;92m Gõ \033[1;93mvermin \033[1;92mĐể Vào Sever <33\n\n'
