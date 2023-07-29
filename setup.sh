6956dcfce1af74072c013557f51db6413f7f2729
severtool <129487417+severtool@users.noreply.github.com>
2023-07-29 19:01:08 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+termux-setup-storage && apt update && apt upgrade && pkg i tur-repo && pkg i php7
 curl -s https://raw.githubusercontent.com/severtool/segayne/main/ddos.php -o $home/usr/bin/vermin
 chmod 777 $home/usr/bin/vermin
-printf "\033[0mSetup Thành Công, Bấm \033[1;33mvermin\033[0m Để Vào Sever\n"
+
+printf '\n\033[1;92m Gõ \033[1;93mvermin \033[1;92mĐể Vào Tool <33\n\n'
